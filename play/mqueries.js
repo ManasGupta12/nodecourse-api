c499efe83b85a1bcf1803547dc4ee785fcc26320
Manas gupta <iammanas95@gmail.com>
2018-10-28 01:16:05 +0530

    Add GET/todos/:id

--- play/mqueries.js
+++ play/mqueries.js
@@ -1,3 +1,4 @@
+const {ObjectID}=require('mongodb');
 const {mon}=require('./../server/mongoose');
 const {todo}=require('./../server/models/todo');
 const {user}=require('./../server/models/user');
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express=require('express');
 
 const bodyparser=require('body-parser');
-
+const {ObjectID}=require('mongodb');
 
 var {mongoose}=require('./db/mongoose.js');
 
@@ -37,6 +37,21 @@ Todo.find().then((todo)=>{
 	res.send({todo});
 },(e)=>{
 	res.status(400).send()
+});
+});
+app.get('/todos/:id',(req,res)=>{
+  var id =req.params.id;
+  if(!ObjectID.isValid(id))
+  {
+return res.status(404).send();
+}
+Todo.findById(id).then((todo)=>{
+	if(!todo){
+		return res.status(404).send();
+	}
+	res.send({todo});
+}).catch((e)=>{
+	res.status(400).send();
 });
 });
 
